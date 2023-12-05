f827409fedeebdc3d29562e2a74d6563cca8c655
tttturtle-russ <u202112205@hust.edu.cn>
2023-12-05 16:54:29 +0800

    add:exp3-4

--- exp3/3-4.c
+++ exp3/3-4.c
@@ -9,21 +9,21 @@ void testFunc() {
     return;
 }
 
-unsigned long v2p(unsigned long vAddr) {
+unsigned long v2p(unsigned long vAddr,unsigned long *PFN) {
     unsigned long data;
     int pagesize = getpagesize();
     int fd = open("/proc/self/pagemap",O_RDONLY);
     unsigned long offset = vAddr / pagesize * sizeof(unsigned long);
     pread(fd,&data,sizeof(unsigned long),offset);
-    unsigned long PFN = data & 0x7fffffffffffff;
-    unsigned long paddr = PFN * pagesize + vAddr % pagesize;
+    *PFN = data & 0x7fffffffffffff;
+    unsigned long paddr = *PFN * pagesize + vAddr % pagesize;
     return paddr;
 }
 
 int main() {
-    unsigned long paddr = v2p((unsigned long)&globalVariable);
-    unsigned long paddr1 = v2p((unsigned long)&testFunc);
-    printf("vaddr:%#p\tpaddr:%#lx\n",&globalVariable,paddr);
-    printf("vaddr:%#p\tpaddr:%#lx\n",&testFunc,paddr1);
-    getchar();
+    unsigned long PFN;
+    unsigned long paddr = v2p((unsigned long)&globalVariable,&PFN);
+    printf("variable:a\tPFN:%#lx\tvaddr:%#p\tpaddr:%#lx\n",PFN,&globalVariable,paddr);
+    paddr = v2p((unsigned long)&testFunc,&PFN);
+    printf("func:testFunc\tPFN:%#lx\tvaddr:%#p\tpaddr:%#lx\n",PFN,&testFunc,paddr);
 }
